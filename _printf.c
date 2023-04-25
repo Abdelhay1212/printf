8b5765ea6e82d6ad212cccd5af1ab6934a024f9f
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-04-25 04:57:26 -0700

    updated

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - prints strings characters and numbers
+ * @format: string to be printed
+ * Return: integer
+ **/
+int _printf(const char *format, ...)
+{
+	int i;
+	char *str;
+	va_list args;
+	char percente;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	percente = '%';
+	i = 0;
+
+	while (format[i])
+	{
+		if (format[i] != '%')
+			write(STDOUT_FILENO, &format[i], sizeof(format[i]));
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			write(STDOUT_FILENO, &percente, sizeof(percente));
+			i++;
+		}
+		else
+		{
+			switch(format[i + 1])
+			{
+				case 'c':
+					char x = va_arg(args, int);
+					write(STDOUT_FILENO, &x, sizeof(x));
+					break;
+				case 's':
+					str = va_arg(args, char*);
+					for (; *str; str++)
+						write(STDOUT_FILENO, str, sizeof(*str));
+					break;
+			}
+			i++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (i);
+}
