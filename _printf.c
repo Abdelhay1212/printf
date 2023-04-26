1e5d2f285c30592e12c9b4df6b8d3e006917de38
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-04-25 19:30:03 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * _printf - prints strings characters and numbers
@@ -9,23 +9,27 @@
  **/
 int _printf(const char *format, ...)
 {
-	int i;
-	char *str, c;
+	int i = 0, count = 0, integer;
+	char *str, *ptr, c;
 	va_list args;
 
 	if (format == NULL)
 		return (-1);
 
+	ptr = malloc(20 * sizeof(char));
 	va_start(args, format);
-	i = 0;
 
 	while (format[i])
 	{
 		if (format[i] != '%')
+		{
 			putchar(format[i]);
+			count++;
+		}
 		else if (format[i] == '%' && format[i + 1] == '%')
 		{
 			putchar('%');
+			count++;
 			i++;
 		}
 		else
@@ -38,16 +42,39 @@ int _printf(const char *format, ...)
 						putchar(c);
 					else
 						putchar(' ');
+					count++;
 					break;
 				case 's':
 					str = va_arg(args, char*);
 					if (str == NULL)
 						str = "(null)";
 					for (; *str; str++)
+					{
 						putchar(*str);
+						count++;
+					}
+					break;
+				case 'd':
+				case 'i':
+					integer = va_arg(args, int);
+					if (integer == '\0')
+					{
+						putchar('0');
+						count++;
+					}
+					else
+					{
+						sprintf(ptr, "%d", integer);
+						for (; *ptr; ptr++)
+						{
+							putchar(*ptr);
+							count++;
+						}
+					}
 					break;
 				default:
 					putchar(format[i]);
+					count++;
 					i--;
 					break;
 			}
@@ -56,5 +83,5 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(args);
-	return (i);
+	return (count);
 }
