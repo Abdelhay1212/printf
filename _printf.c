fc19e8987127bedc99d84614eb197e90c2f7f4c2
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-04-25 20:40:26 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -3,6 +3,38 @@
 #include <stdlib.h>
 
 /**
+  * print_hex - print hexadecimal numbers
+  * @n: hexadicimal
+  * @count: length of decimals
+  * @charType: character
+  * Return: integer
+  */
+int print_hex(unsigned int n, int count, char charType)
+{
+	if (n >= 16)
+	{
+		count = print_hex(n / 16, count, charType);
+	}
+
+	int digit = n % 16;
+
+	if (digit < 10)
+	{
+		putchar('0' + digit);
+        	count++;
+	}
+	else
+	{
+		if (charType == 'x')
+			putchar('a' + digit - 10);
+		else
+			putchar('A' + digit - 10);
+		count++;
+	}
+	return (count);
+}
+
+/**
  * _printf - prints strings characters and numbers
  * @format: string to be printed
  * Return: integer
@@ -10,6 +42,7 @@
 int _printf(const char *format, ...)
 {
 	int i = 0, count = 0, integer;
+	unsigned int hex;
 	char *str, *ptr, c;
 	va_list args;
 
@@ -71,6 +104,12 @@ int _printf(const char *format, ...)
 							count++;
 						}
 					}
+					break;
+				case 'x':
+				case 'X':
+					c = format[i + 1];
+					hex = va_arg(args, unsigned int);
+					count = print_hex(hex, count, c);
 					break;
 				default:
 					putchar(format[i]);
